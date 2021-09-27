a069c6df035536a18178c30d548352a36ed87272
Marius <grausas@gmail.com>
2021-09-27 10:58:22 +0300

    button component created

---
+++ src/components/Button/Button.js
@@ -0,0 +1,7 @@
+import { Wrapper } from "./Button.style";
+
+const Button = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
+
+export default Button;
---
+++ src/components/Button/Button.style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.button`
+  background: var(--blue);
+  color: var(--white);
+  padding: 0.5rem 1.2rem;
+  font-size: var(--font-small);
+  font-weight: 600;
+  border-radius: 10px;
+  border: 1px solid var(--color-light);
+  cursor: pointer;
+  :hover {
+    background: var(--white);
+    color: var(--blue);
+    border: 1px solid var(--color-dark);
+    transition: ease-in-out 0.2s;
+  }
+`;
