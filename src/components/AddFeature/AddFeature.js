da615c238ea040f7dd94e2b91a1e4c9e8f08525c
Marius <grausas@gmail.com>
2021-10-04 12:57:22 +0300

    add feature structure changed

--- src/components/AddFeature/AddFeature.js
+++ src/components/AddFeature/AddFeature.js
@@ -1,15 +1,35 @@
 // Styles
-import { Wrapper, InputWrapper } from "./AddFeature.style";
+import {
+  Close,
+  FormWrapper,
+  InputWrapper,
+  CloseImage,
+} from "./AddFeature.style";
+// hooks
+import { useOpenClose } from "../../hooks/useOpenClose";
+//icons
+import CloseIcon from "../../assets/icons/close.png";
 
 const AddFeature = ({ children, handleSubmit, titleText, buttonText }) => {
+  const { handleOpen, show } = useOpenClose();
+
   return (
-    <Wrapper>
-      <form onSubmit={handleSubmit}>
-        <h3>{titleText}</h3>
-        <InputWrapper>{children}</InputWrapper>
-        <button type="submit">{buttonText}</button>
-      </form>
-    </Wrapper>
+    <div>
+      {!show ? (
+        <Close>
+          <span onClick={() => handleOpen(show)}>Add</span>
+        </Close>
+      ) : (
+        <FormWrapper>
+          <form onSubmit={handleSubmit}>
+            <h3>{titleText}</h3>
+            <CloseImage src={CloseIcon} alt="close-icon" onClick={handleOpen} />
+            <InputWrapper>{children}</InputWrapper>
+            <button type="submit">{buttonText}</button>
+          </form>
+        </FormWrapper>
+      )}
+    </div>
   );
 };
 
--- src/components/AddFeature/AddFeature.style.js
+++ src/components/AddFeature/AddFeature.style.js
@@ -1,19 +1,48 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Close = styled.div`
   position: absolute;
-  width: 350px;
   max-height: 600px;
   top: 2%;
   border-radius: 5px;
   right: 20px;
+  padding: 5px 10px;
+  background: var(--grey);
+  color: var(--white);
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+export const FormWrapper = styled.div`
+  position: absolute;
+  width: 350px;
+  max-height: 600px;
+  border-radius: 5px;
+  top: 2%;
+  right: 20px;
   background-color: var(--white);
+  color: var(--dark);
   box-shadow: 0px 5px 60px 0px rgba(0, 0, 0, 0.25);
   padding: 20px;
+
+  h3 {
+    margin-bottom: 10px;
+  }
 `;
 
 export const InputWrapper = styled.div`
   width: 100%;
   margin-bottom: 20px;
   text-align: left;
+`;
+
+export const CloseImage = styled.img`
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  width: 22px;
+  height: 22px;
+  border-radius: 5px;
+  padding: 3px;
+  cursor: pointer;
 `;
