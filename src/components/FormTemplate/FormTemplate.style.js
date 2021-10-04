8843eead601adfe729ad7171ea5b81b566f8e4eb
Marius <grausas@gmail.com>
2021-10-04 10:27:38 +0300

    form template created

---
+++ src/components/FormTemplate/FormTemplate.style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  position: absolute;
+  width: 350px;
+  max-height: 600px;
+  top: 2%;
+  border-radius: 5px;
+  right: 20px;
+  background-color: var(--white);
+  box-shadow: 0px 5px 60px 0px rgba(0, 0, 0, 0.25);
+  padding: 20px;
+`;
+
+export const InputWrapper = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+  text-align: left;
+`;
