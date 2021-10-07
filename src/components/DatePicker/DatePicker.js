5309fffb1f63f92a0669b6c0026c8b2b29fbbf51
Marius <grausas@gmail.com>
2021-10-07 10:51:44 +0300

    date picker changed locale

--- src/components/DatePicker/DatePicker.js
+++ src/components/DatePicker/DatePicker.js
@@ -1,21 +1,30 @@
 // date picker
-import SingleDatePicker from "react-datepicker";
+import SingleDatePicker, { registerLocale } from "react-datepicker";
+import lt from "date-fns/locale/lt";
 import "react-datepicker/dist/react-datepicker.css";
 
 // styles
 import { Wrapper } from "./DatePicker.style";
 
+registerLocale("lt", lt);
+
 const DatePicker = ({ selected, handleChange }) => {
   return (
     <Wrapper>
       <SingleDatePicker
-        imeInputLabel="Laikas:"
+        locale="lt"
+        timeInputLabel="Laikas:"
+        timeCaption="Laikas"
+        //   showMonthDropdown
+        //   showYearDropdown
+        showTimeSelect
         timeFormat="HH:mm"
         timeIntervals={1}
         dateFormat="yyyy/MM/dd HH:mm"
-        showTimeSelect
+        show
         selected={selected}
         onChange={handleChange}
+        dropdownMode="select"
       />
     </Wrapper>
   );
