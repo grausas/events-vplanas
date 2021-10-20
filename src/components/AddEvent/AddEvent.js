141ba1cbfba2dc04fd237faaadabaadbeb718418
Marius <grausas@gmail.com>
2021-10-20 10:27:36 +0300

    favicon changed

--- src/components/AddEvent/AddEvent.js
+++ src/components/AddEvent/AddEvent.js
@@ -33,45 +33,43 @@ const AddEvent = ({
           <span onClick={handleOpen}>Pridėti objektą</span>
         </Close>
       ) : (
-        <>
-          <FormWrapper isEditing={isEditing}>
-            <CloseImage src={CloseIcon} alt="close-icon" onClick={handleOpen} />
-            {!isEditing ? (
-              <>
+        <FormWrapper isEditing={isEditing}>
+          <CloseImage src={CloseIcon} alt="close-icon" onClick={handleOpen} />
+          {!isEditing ? (
+            <>
+              <ConfirmButton
+                handleClick={() => {
+                  handleUpdate();
+                }}
+              >
+                Redaguoti objektą
+              </ConfirmButton>
+              <form onSubmit={handleSubmit}>
+                <h3>{titleText}</h3>
+                <InputWrapper>{children}</InputWrapper>
+                <ConfirmButton>{buttonText}</ConfirmButton>
                 <ConfirmButton
                   handleClick={() => {
-                    handleUpdate();
+                    handleCancel();
                   }}
                 >
-                  Redaguoti objektą
+                  {buttonTitleCancel}
                 </ConfirmButton>
-                <form onSubmit={handleSubmit}>
-                  <h3>{titleText}</h3>
-                  <InputWrapper>{children}</InputWrapper>
-                  <ConfirmButton>{buttonText}</ConfirmButton>
-                  <ConfirmButton
-                    handleClick={() => {
-                      handleCancel();
-                    }}
-                  >
-                    {buttonTitleCancel}
-                  </ConfirmButton>
-                </form>
-              </>
-            ) : (
-              <>
-                <p>{spanText}</p>
-                <ConfirmButton
-                  handleClick={() => {
-                    handleCordinates();
-                  }}
-                >
-                  {buttonTitle}
-                </ConfirmButton>
-              </>
-            )}
-          </FormWrapper>
-        </>
+              </form>
+            </>
+          ) : (
+            <>
+              <p>{spanText}</p>
+              <ConfirmButton
+                handleClick={() => {
+                  handleCordinates();
+                }}
+              >
+                {buttonTitle}
+              </ConfirmButton>
+            </>
+          )}
+        </FormWrapper>
       )}
     </div>
   );
