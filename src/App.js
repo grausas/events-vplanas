c9f1bc6c22af7029431561b9581a3fdc1d20d517
Marius <grausas@gmail.com>
2022-01-12 15:14:40 +0200

    routes added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-// pages
-import Map from "./pages/Map";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Components
 import { Header } from "./components/index";
+// pages
+import Map from "./pages/Map";
+import Login from "./pages/Login";
+// const HomeLazy = lazy(() => import("./pages/Map"));
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Map />
-    </>
+      {/* <Map /> */}
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/" element={<Map />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
