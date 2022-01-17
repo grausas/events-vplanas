7c7046e0d22a0d175dd40f6317df4ca6b615cb29
Marius <grausas@gmail.com>
2022-01-17 16:23:04 +0200

    fixed routes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import { Header } from "./components/index";
 // pages
 import Map from "./pages/Map";
 import Login from "./pages/Login";
-// const HomeLazy = lazy(() => import("./pages/Map"));
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/renginiai">
       <Header />
-      {/* <Map /> */}
       <Routes>
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/" element={<Map />} />
