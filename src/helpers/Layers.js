04a1bf47aa74b974bf3f3d0c9cae2e7293b5c3c7
Marius <grausas@gmail.com>
2022-01-19 14:29:04 +0200

    private layer added

--- src/helpers/Layers.js
+++ src/helpers/Layers.js
@@ -2,9 +2,20 @@ import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import TileLayer from "@arcgis/core/layers/TileLayer";
 import VectorLayer from "@arcgis/core/layers/VectorTileLayer";
 
-export const featureLayer = () => {
+export const featureLayerPrivate = () => {
   const layer = new FeatureLayer({
     url: "https://services1.arcgis.com/usA3lHW20rGU6glp/ArcGIS/rest/services/Renginiai_Vilniuje_P/FeatureServer/0",
+    outFields: ["*"],
+    // rodyti renginius nuo dabartinės datos
+    // definitionExpression:
+    //   "RENGINIO_PRADZIA > date'" + new Date().toISOString().slice(0, 10) + "'",
+  });
+  return layer;
+};
+
+export const featureLayer = () => {
+  const layer = new FeatureLayer({
+    url: "https://services1.arcgis.com/usA3lHW20rGU6glp/arcgis/rest/services/Renginiai_Vilniuje_P_View/FeatureServer",
     outFields: ["*"],
     // rodyti renginius nuo dabartinės datos
     // definitionExpression:
