51153ac4c80daa9d1f9e6e9f5248f16e44e493cf
Marius <grausas@gmail.com>
2022-01-28 11:12:48 +0200

    map styled compoenent file created

---
+++ src/pages/Map.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MapDiv = styled.div`
+  width: 100%;
+  height: calc(100% - 50px);
+  position: relative !important;
+`;
+
+export const SearchDiv = styled.div`
+  position: absolute;
+  right: 0;
+  top: 100px;
+`;
