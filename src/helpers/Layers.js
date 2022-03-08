5d9543f6edab6fe604c7b88ea59b9c94cf93a426
Marius <grausas@gmail.com>
2022-03-08 12:00:39 +0200

    changed date to query max events

--- src/helpers/Layers.js
+++ src/helpers/Layers.js
@@ -1,14 +1,13 @@
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
-const date = new Date().toISOString();
-// const queryEventsDate = new Date(
-//   date.setMonth(date.getMonth() - 1)
-// ).toISOString();
+const date = new Date();
+const publicDate = new Date(date.setMonth(date.getMonth() - 1)).toISOString();
+const privateDate = new Date(date.setMonth(date.getMonth() - 6)).toISOString();
 
 export const featureLayer = () => {
   const layer = new FeatureLayer({
     url: "https://services1.arcgis.com/usA3lHW20rGU6glp/arcgis/rest/services/Renginiai_Vilniuje_P_View/FeatureServer/0",
-    definitionExpression: "RENGINIO_PABAIGA > '" + date + "'",
+    definitionExpression: "RENGINIO_PABAIGA > '" + publicDate + "'",
     outFields: ["*"],
     title: "public",
   });
@@ -18,7 +17,7 @@ export const featureLayer = () => {
 export const featureLayerPrivate = () => {
   const layer = new FeatureLayer({
     url: "https://services1.arcgis.com/usA3lHW20rGU6glp/ArcGIS/rest/services/Renginiai_Vilniuje_P/FeatureServer/0",
-    definitionExpression: "RENGINIO_PABAIGA > '" + date + "'",
+    definitionExpression: "RENGINIO_PABAIGA > '" + privateDate + "'",
     outFields: ["*"],
     title: "private",
   });
